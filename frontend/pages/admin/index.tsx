5cf09ca5e1ad5559a6463dca38a0d8ee2b726124
Martin Clementz <martcl@stud.ntnu.no>
2022-03-15 22:53:41 +0100

    feat(fileUploader): added basic file uploader

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,12 +1,14 @@
 import { Row } from "@nextui-org/react";
 import type { NextPage } from "next";
 import AdminNavbar from "../../components/AdminNavbar";
+import FileUploader from "../../components/FileUploader";
 import LoginProvider from "../../components/LoginProvider";
 
 const AdminPage: NextPage = () => {
   return (
     <LoginProvider>
       <AdminNavbar />
+      <FileUploader />
       <Row css={{ height: "100vh" }} justify="center" align="center">
         Du er logget inn
       </Row>
---
+++ frontend/types.tsx
@@ -0,0 +1,9 @@
+export type attendantType = {
+  name: String;
+  mail: String;
+  group: String;
+  ticket_sent: Boolean;
+  registered: Boolean;
+  attended: Boolean;
+  foto_consent: Boolean;
+};
